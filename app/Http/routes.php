2bcb0dbb2fc8ede58a6120ed144297b013c843fc
Dalvani <dalvani@gmail.com>
2015-11-27 18:23:26 -0200

    - Fase 3 - Criando Autenticacao para API

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -55,3 +55,19 @@ Route::group(['prefix'=>'customer', 'middleware'=> 'auth.checkrole:client', 'as'
     Route::get('order/create', ['as'=>'order.create', 'uses'=>'CheckoutController@create']);
     Route::post('order/store', ['as'=>'order.store', 'uses'=>'CheckoutController@store']);
 });
+
+Route::post('oauth/access_token', function() {
+    return Response::json(Authorizer::issueAccessToken());
+});
+
+
+Route::group(['prefix'=>'api', 'middleware'=> 'oauth','as'=>'api.'], function() {
+    Route::get('teste', function(){
+        return [
+            'id' => 1,
+            'client' => 'Luiz Carlos',
+            'total' => 10
+        ];
+    });
+});
+
